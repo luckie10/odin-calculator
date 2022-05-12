78229a296053230d2826519f3b9bb278b5d020e3
Boban Kilvin <btrajcevski10@gmail.com>
2022-05-12 15:24:16 -0400

    Add display functionality when numbers are clicked

--- scripts.js
+++ scripts.js
@@ -1,8 +1,31 @@
-const calculator = {
+const operators = {
 	add: function (a, b) { return a + b; },
 	subtract: function (a, b) { return a - b; },
 	multiply: function (a, b) { return a * b; },
 	divide: function (a, b) { return a / b;	},
 	
-	operate: function (operator, a, b) { return calculator[operator](a, b); }
+	operate: function (operator, a, b) { return this[operator](a, b); }
 }
+
+const numberButtons = document.querySelectorAll('.number');
+const display = document.getElementById('display');
+
+let displayValue = '0';
+
+function updateDisplay(content) {
+	display.textContent = content;
+}
+
+function clickNumber(event) {
+	if (displayValue === '0') {
+		displayValue = event.target.id;
+	} else {
+		displayValue += event.target.id;
+	}
+
+	updateDisplay(displayValue);
+}
+
+numberButtons.forEach(button => {
+	button.addEventListener('click', clickNumber);
+});
