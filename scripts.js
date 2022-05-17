b09a2574e73dfd930b27f9f51637278bb1fbf3fd
Boban Kilvin <btrajcevski10@gmail.com>
2022-05-17 05:16:40 -0400

    Add equals and clear button functionality

--- scripts.js
+++ scripts.js
@@ -5,13 +5,16 @@ const OPERATORS = {
 	'/': 'divide',
 }
 
+let activeOperator;
 let firstOperand;
 let secondOperand;
 
 const numberButtons = document.querySelectorAll('.number');
 const operatorButtons = document.querySelectorAll('.operator');
+const equalsButton = document.querySelector('.equals');
+const clearButton = document.getElementById('clear');
 const equationDisplay = document.getElementById('equationDisplay');
-const resultDisplay = document.getElementById('resultDisplay');
+const mainDisplay = document.getElementById('mainDisplay');
 
 const operatorFunctions = {
 	add: function (a, b) { return a + b; },
@@ -19,21 +22,41 @@ const operatorFunctions = {
 	multiply: function (a, b) { return a * b; },
 	divide: function (a, b) { return a / b;	},
 	
-	operate: function (operator, a, b) { return this[operator](a, b); }
+	operate: function (operator, a, b) { 
+		a = Number(a);
+		b = Number(b);
+		
+		return this[operator](a, b);
+	}
 }
 
 function appendNumber(number) {
-	if (!equationDisplay.textContent) {
-		equationDisplay.textContent = number;
+	if (!mainDisplay.textContent || activeOperator) {
+		mainDisplay.textContent = number;
 	} else {
-		equationDisplay.textContent += number;
+		mainDisplay.textContent += number;
 	}
 }
 
 function appendOperator(operator) {
-	if (!equationDisplay.textContent) return
+	if (!mainDisplay.textContent) return
+	if (activeOperator) equals();
 
-	equationDisplay.textContent = `${equationDisplay.textContent} ${operator} `
+	firstOperand = mainDisplay.textContent;
+	activeOperator = operator;
+
+	equationDisplay.textContent = `${mainDisplay.textContent} ${operator} `
+}
+
+function equals() {
+	secondOperand = mainDisplay.textContent;
+
+	equationDisplay.textContent = 
+			`${firstOperand} ${activeOperator} ${secondOperand} =`;
+	mainDisplay.textContent = operatorFunctions
+			.operate(OPERATORS[activeOperator], firstOperand, secondOperand);
+
+	activeOperator = null;
 }
 
 operatorButtons.forEach(button => {
@@ -42,4 +65,13 @@ operatorButtons.forEach(button => {
 
 numberButtons.forEach(button => {
 	button.addEventListener('click', () => appendNumber(button.textContent));
+});
+
+equalsButton.addEventListener('click', equals);
+clearButton.addEventListener('click', () => {
+	equationDisplay.textContent = '';
+	mainDisplay.textContent = '';
+	firstOperand = '';
+	secondOperand = '';
+	activeOperator = null;
 });
